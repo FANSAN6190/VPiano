71ab60434e77e4aaa36a6b45de7756ed8a1293c1
FANSAN6190 <fanin.s.pbl@gmail.com>
2023-10-06 18:18:32 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var i;
-for(i=0;i<document.querySelectorAll(".key").length;i++){
-    document.querySelectorAll("button")[i].addEventListener("click",function(){
+for(i=0;i<document.querySelectorAll(".keys").length;i++){
+    document.querySelectorAll(".keys")[i].addEventListener("click",function(){
         console.log("click : "+this.innerHTML);
         var buttonInnerHTML=this.innerHTML;
         makeSound(buttonInnerHTML);
@@ -15,37 +15,78 @@ for(i=0;i<document.querySelectorAll(".key").length;i++){
 function makeSound(inp){
     buttonAnimation(inp);
     switch(inp){                                                                                                   
-        case "w":
-            var tom1=new Audio("sounds/tom-1.mp3");
+        case "e":
+            var tom1=new Audio("assets/audio/25405__tedagame__88-piano-keys-long-reverb/448532__tedagame__c5.ogg");
             tom1.play();
             break;
-        case "a":
-            var tom2=new Audio("sounds/tom-2.mp3");
+        case "4":
+            var tom2=new Audio("assets/audio/25405__tedagame__88-piano-keys-long-reverb/448532__tedagame__c5.ogg");
             tom2.play();
             break;
-        case "s":
+        case "r":
             var tom3=new Audio("sounds/tom-3.mp3"); 
             tom3.play();
             break;
-        case "d":
+        case "5":
             var tom4=new Audio("sounds/tom-4.mp3");
             tom4.play();
             break;
-        case "j":
+        case "t":
             var crash=new Audio("sounds/crash.mp3");
             crash.play();
             break;
-        case "k":
+        case "y":
             var kick=new Audio("sounds/kick-bass.mp3");
             kick.play();
             break;
-        case "l":
+        case "7":
             var snare=new Audio("sounds/snare.mp3");
             snare.play();
             break;
-        default:
-            console.log("Unknown"+buttonInnerHTML);
+        case "u":
+            var tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play();
             break;
+        case "8":
+            var crash=new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "i":
+            var kick=new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "9":
+            var snare=new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        
+        case "o":
+            var tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "p":
+            var crash=new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "-":
+            var kick=new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "[":
+            var snare=new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "=":
+            var tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "]":
+            var crash=new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        default:
+        console.log("Unknown"+buttonInnerHTML);
+        break;
     };
 }
 function buttonAnimation(currentKey){
