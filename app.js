47bb6ecc3d5e76ae5f8435824d1a86128a322d68
FANSAN6190 <fanin.s.pbl@gmail.com>
2023-10-09 00:15:31 +0530

    added recording feature (still needs some work), inproved piano design

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var i;
-
 for(i=0;i<document.querySelectorAll(".keys").length;i++){
     let key=document.querySelectorAll(".keys")[i];
     //key.querySelector("h4").innerHTML=key.id;
@@ -24,9 +23,6 @@ for(i=0;i<document.querySelectorAll(".keys").length;i++){
             }
         };  
     });
-}
-function note_label(id){
-    return (document.getElementById(id).querySelector("h4").innerHTML);
 }
 
 function makeSound(inp,inp_type){
@@ -217,3 +213,52 @@ function buttonAnimation(currentKey,inp_type){
     }
     keysCheckbox.addEventListener("click", showHideKeys);
 }   
+
+
+
+
+var startTime; // to keep track of the start time
+var stopwatchInterval; // to keep track of the interval
+var elapsedPausedTime = 0; // to keep track of the elapsed time while stopped
+
+
+function startStopwatch() {
+  if (!stopwatchInterval) {
+    startTime = new Date().getTime() - elapsedPausedTime; // get the starting time by subtracting the elapsed paused time from the current time
+    stopwatchInterval = setInterval(updateStopwatch, 1000); // update every second
+  }
+}
+function startStopwatch() {
+  if (!stopwatchInterval) {
+    startTime = new Date().getTime() - elapsedPausedTime; // get the starting time by subtracting the elapsed paused time from the current time
+    stopwatchInterval = setInterval(updateStopwatch, 1000); // update every second
+  }
+}
+
+
+function stopStopwatch() {
+  clearInterval(stopwatchInterval); // stop the interval
+  elapsedPausedTime = new Date().getTime() - startTime; // calculate elapsed paused time
+  stopwatchInterval = null; // reset the interval variable
+}
+
+function resetStopwatch() {
+  stopStopwatch(); // stop the interval
+  elapsedPausedTime = 0; // reset the elapsed paused time variable
+  document.getElementById("timer").innerHTML = "00:00:00"; // reset the display
+}
+
+function updateStopwatch() {
+    var currentTime = new Date().getTime(); // get current time in milliseconds
+    var elapsedTime = currentTime - startTime; // calculate elapsed time in milliseconds
+    var seconds = Math.floor(elapsedTime / 1000) % 60; // calculate seconds
+    var minutes = Math.floor(elapsedTime / 1000 / 60) % 60; // calculate minutes
+    var hours = Math.floor(elapsedTime / 1000 / 60 / 60); // calculate hours
+    var displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds); // format display time
+    document.getElementById("timer").innerHTML = displayTime; // update the display
+}
+
+function pad(number) {
+  // add a leading zero if the number is less than 10
+  return (number < 10 ? "0" : "") + number;
+}
--- record.js
+++ record.js
@@ -1,10 +1,36 @@
-var music=[];
-rec=false;
+var music=['.'];
+var rec=false;
+
+function note_label(id){
+    return (document.getElementById(id).querySelector("h4").innerHTML);
+}
 var i=1;
 function record(){
-    recording=setInterval(() => {
-        var note='.';
+    startStopwatch();
+    rec=true;
+    var recording = setInterval(() => {
+        if(rec==false){
+            clearInterval(recording);
+            savedata();
+        }
+        music.push('.');
+
+        onkeydown=(event)=>{
+            if(event.repeat){
+                music.push('_')
+                return;
+            }
+            music.push(note_label(event.key));
+        }
         console.log(music);
-    }, 100);
-    alert("hello")
+        
+    }, 200);
+}
+
+function savedata() {
+    var filename=prompt("Enter filename : ")
+    var blob = new Blob([music.join(" ")],
+        { type: "application/octet-stream;charset=utf-8" });
+    saveAs(blob, filename);
+    resetStopwatch();
 }
